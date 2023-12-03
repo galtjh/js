e1f1312fde4cd2123b51ab2a150c0b183eb2b169
glt <148288521+galtjh@users.noreply.github.com>
2023-12-03 19:30:38 +0530

    Update javascript.js
    JavaScript is wrapped within html file

--- javascript.js
+++ javascript.js
@@ -1,28 +1,38 @@
-let a = 7;
-let b = 4;
-console.log('The product of two variables is:'),
-console.log(a * b);
+<!doctype html>
+<html>
+    <head>
+        <title>JavaScript</title>
+    </head>
 
-//Multiple variables 
-var obj = {
-    x: 1,
-    y: 2,
-    z: 3
-};
-console.log('Access properties via dot notation:'); 
-console.log(obj.x, obj.y, obj.z);
+    <body>
+        <script> 
+            let a = 7;
+            let b = 4;
+            console.log('The product of two variables is:'),
+            console.log(a * b);
 
+            //Multiple variables 
+            var obj = {
+             x: 1,
+            y: 2,
+            z: 3
+            };
+            console.log('Access properties via dot notation:'); 
+            console.log(obj.x, obj.y, obj.z);
 
+            // Assuming obj is an object with properties x, y, and z
+            var obj = {
+            x: 4,
+            y: 5,
+            z: 6
+            };
 
-// Assuming obj is an object with properties x, y, and z
-var obj = {
-    x: 4,
-    y: 5,
-    z: 6
-};
+            with (obj) {
+            console.log("Access properties within the 'with' block:");
+            console.log(x, y, z);
+            }
 
-with (obj) {
-    console.log("Access properties within the 'with' block:");
-    console.log(x, y, z);
-}
+        </script>
+    </body>
+</html>
 
